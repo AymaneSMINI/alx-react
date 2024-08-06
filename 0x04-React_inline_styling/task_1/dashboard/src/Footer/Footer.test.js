759f376927da20e2729962508cad4d399619f91c
aymane smini <aymane.smini@gmail.com>
2024-08-06 19:50:52 +0100

    install Aphrodite

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Footer/Footer.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Footer from "./Footer";
+
+jest.mock("../utils/utils", () => ({
+  getFullYear: jest.fn(() => 2024),
+  getFooterCopy: jest.fn(() => "Copyright"),
+}));
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Footer Component", () => {
+  it("renders without crashing", () => {
+    render(<Footer />);
+  });
+
+  it("renders img and h1 tags", () => {
+    const { getByText } = render(<Footer />);
+    const copyrightText = getByText(/Current year: 2024-Copyright/i);
+    expect(copyrightText).toBeInTheDocument();
+  });
+});
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/utils/utils.test.js
@@ -0,0 +1,37 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from "./utils";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Get full year", function () {
+  it("should return the correct year", function () {
+    const currentYear = new Date().getFullYear();
+    expect(getFullYear()).toBe(currentYear);
+  });
+});
+
+describe("Get footer message", function () {
+  const trueMessage = "Holberton School";
+  const falseMessage = "Holberton School main dashboard";
+
+  it("should return the true footer message", function () {
+    const currentMessage = getFooterCopy(true);
+    expect(currentMessage).toBe(trueMessage);
+  });
+  it("should return the false footer message", function () {
+    const currentMessage = getFooterCopy(false);
+    expect(currentMessage).toBe(falseMessage);
+  });
+});
+
+describe("Get latest notification", function () {
+  it("should return the urgent message text", function () {
+    const message = "<strong>Urgent requirements</strong> - complete by EOD";
+    expect(message).toBe(getLatestNotification());
+  });
+});
